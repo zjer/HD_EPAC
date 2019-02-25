88db12fdc9b05051b104f8f784496e135303d0bd
shujin <15651026970@163.com>
2019-02-25 14:15:22 +0800

    service初始化

---
+++ Service/src/main/java/com/hd/epac/EpacApplication.java
@@ -0,0 +1,13 @@
+package com.hd.epac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpacApplication.class, args);
+    }
+
+}
